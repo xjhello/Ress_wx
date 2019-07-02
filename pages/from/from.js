71d032e6029661147dbe39575dcde2eeaf1833f4
xu <824949896@qq.com>
2019-07-02 16:54:21 +0800

    2019.7.2小程序(登录功能完成)

---
+++ pages/from/from.js
@@ -0,0 +1,100 @@
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    uname:'',
+    mydata:{},
+    eqList:[]
+  },
+
+  
+  // 获取数据
+  getData:function(e){
+    var that = this;
+    wx.request({
+      url: 'http://47.100.12.130:3111/api/ims/page?page=5&size=20', //仅为示例，并非真实的接口地址
+      method: 'GET',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        that.mydata = res.data
+        console.log(that.mydata)
+        that.setData({
+          mydata: res.data
+        })
+      }
+    })
+  },
+
+
+  // 获取数据
+  getDataByName: function (uid) {
+    var that = this;
+    var urls = ''
+    console.log('!!!!!!!!!!:' + uid)
+    urls = 'http://192.168.1.104:3111/api/userDevice?username=' + uid
+    console.log(urls)
+    wx.request({
+      url: 'http://192.168.1.104:3111/api/userDevice?username=' + uid,
+      method: 'GET',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res){
+        console.log(res.data)
+        that.setData({
+          eqList: res.data['data']
+        })
+      }
+    })
+    
+  },
+
+
+  onLoad: function (options) {
+    var uid = options.id
+    console.log('uname!!!!!!!!!!!!!:' + uid)
+    this.setData({
+      uname:uid
+    })
+    this.getDataByName(uid)
+    
+  },
+
+ 
+  onReady: function () {
+
+  },
+
+  
+  onShow: function () {
+
+  },
+
+  
+  onHide: function () {
+
+  },
+
+  
+  onUnload: function () {
+
+  },
+
+  
+  onPullDownRefresh: function () {
+
+  },
+
+ 
+  onReachBottom: function () {
+
+  },
+
+  
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,134 @@
+//index.js
+//获取应用实例
+var hsha512  = require('sha512.js')
+var CusBase64 = require('base64.js');
+//函数调用
+// 
+const app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+
+  // 用户名检测
+  unameTest:function(id){
+    console.log('111111111111111')
+    wx.request({
+      url: 'http://www.swisys.com.cn:3111/api/ims/getValidateCode', 
+      method: 'POST',
+      data: {
+        name: id,
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success(res) {
+        console.log('111111111111111')
+        console.log(res.data['salt'])
+        return res.data['salt'] 
+      }
+    })
+
+    
+  },
+
+    // 用户登录
+  userLogin: function (e) {
+    var id = e.detail.value.id
+    var pwd = e.detail.value.pwd
+    var salt = ''
+
+    // 得到salt
+    wx.request({
+      url: 'http://www.swisys.com.cn:3111/api/ims/getValidateCode',
+      method: 'POST',
+      data: {
+        name: id,
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success(res) {
+        console.log('成功得到salt！！！')
+        console.log('salt!!!'+res.data['salt'])
+        salt =  res.data['salt']
+        console.log('salt:' + salt)
+        var newPwd = pwd + salt
+        console.log('newPwd:' + newPwd)
+        var newSha512 = hsha512.sha512(newPwd)
+        console.log('newSha512:' + newSha512)
+        var newBase64 = CusBase64.CusBASE64.encoder(newSha512);
+        // const arrayBuffer = new Uint8Array([newSha512])
+        // const newBase64 = wx.arrayBufferToBase64(arrayBuffer)
+        console.log('newBase64:' + newBase64)
+        wx.request({
+          url: 'http://47.100.12.130:3111/api/ims/checkPassword',
+          method: 'POST',
+          data: {
+            name: id,
+            password: newBase64
+          },
+          header: {
+            'content-type': 'application/x-www-form-urlencoded'
+          },
+          success(res) {
+            console.log(res.data)
+            wx.navigateTo({
+              url: '../from/from?id=' + id,
+            })
+          }
+        })
+
+      }
+    })
+    
+  },
+
+
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
